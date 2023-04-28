533b0a53c6354fb19924a21e6bb6f202995f28a1
Lisa Kraisriwatana <humanwave11@gmail.com>
2023-04-28 15:38:49 -0400

    added select feature, created moodtools to declutter components

--- client/components/CalendarPage/MoodCalendar.js
+++ client/components/CalendarPage/MoodCalendar.js
@@ -3,36 +3,16 @@ import { Box } from '@mui/material';
 import Calendar from 'react-calendar';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllMoods } from '../../store/slices/moods';
-import { styled } from '@mui/system';
+import { formatCalendarDate, CalendarTile } from './moodTools';
 
-const formatCalendarDate = (date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-};
-
-const CalendarTile = styled('div')(({ theme }) => ({
-  '.react-calendar__tile': {
-    width: '4rem',
-    height: '4rem',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  '.mood-Elated': { backgroundColor: '#FF69B4' },
-  '.mood-Happy': { backgroundColor: '#FF95C0' },
-  '.mood-Content': { backgroundColor: '#FFC0CB' },
-  '.mood-So-so': { backgroundColor: '#DCDCDC' },
-  '.mood-Unhappy': { backgroundColor: '#A9A9A9' },
-  '.mood-Despondent': { backgroundColor: '#555555' },
-}));
-
-const MoodCalendar = ({ initialValue, onDateChange }) => {
+const MoodCalendar = ({ onDateChange }) => {
   const [value, setValue] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(null);
   const moods = useSelector((state) => state.moods.allMoods);
   const dispatch = useDispatch();
+  const [highlightedDate, setHighlightedDate] = useState(
+    formatCalendarDate(new Date())
+  );
 
   useEffect(() => {
     dispatch(fetchAllMoods());
@@ -40,6 +20,8 @@ const MoodCalendar = ({ initialValue, onDateChange }) => {
 
   const handleDateChange = (date) => {
     const formattedDate = formatCalendarDate(date);
+    setHighlightedDate(formattedDate);
+
     const mood = moods.find((mood) => mood.date === formattedDate);
 
     setSelectedDate(mood ? formattedDate : null);
@@ -57,7 +39,14 @@ const MoodCalendar = ({ initialValue, onDateChange }) => {
 
   const getTileClassName = ({ date }) => {
     const mood = getMoodForDate(date);
-    return mood ? `mood-${mood}` : 'no-mood';
+    const formattedDate = formatCalendarDate(date);
+    const classes = [mood ? `mood-${mood}` : 'no-mood'];
+
+    if (formattedDate === highlightedDate) {
+      classes.push('highlighted-date');
+    }
+
+    return classes.join(' ');
   };
 
   return (
--- client/components/CalendarPage/MoodForm.js
+++ client/components/CalendarPage/MoodForm.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Box, Button, Snackbar, Typography } from '@mui/material';
-import { fetchAllMoods, createMood } from '../../store/slices/moods';
+import { createMood } from '../../store/slices/moods';
+import { moodButtons } from './moodTools';
 
 const MoodForm = (props) => {
   const { formattedDate } = props;
@@ -9,12 +10,6 @@ const MoodForm = (props) => {
   const user = useSelector((state) => state.auth.user);
   const [selectedMood, setSelectedMood] = useState(null);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-
-  useEffect(() => {
-    if (formattedDate) {
-      dispatch(fetchAllMoods(formattedDate));
-    }
-  }, [dispatch, formattedDate]);
 
   const handleMoodClick = (mood) => {
     setSnackbarOpen(true);
@@ -27,30 +22,6 @@ const MoodForm = (props) => {
       })
     );
   };
-
-  const moodButtons = [
-    { mood: 'Elated', url: 'https://i.gifer.com/JSQ.gif' },
-    {
-      mood: 'Happy',
-      url: 'https://i.gifer.com/origin/ff/ff95320f2d482b0d3847efd6842c7d39_w200.gif',
-    },
-    {
-      mood: 'Content',
-      url: 'https://www.pngkit.com/png/full/349-3491873_hamtaro-pixel-hamster.png',
-    },
-    {
-      mood: 'So-so',
-      url: 'https://i.gifer.com/origin/fe/fea01b80b4d059622a8bba8e3e1655fc_w200.gif',
-    },
-    {
-      mood: 'Unhappy',
-      url: 'https://i.gifer.com/origin/5e/5e3bb1bffaa326ef4e12916d4e506829_w200.gif',
-    },
-    {
-      mood: 'Despondent',
-      url: 'https://64.media.tumblr.com/07ad0e204239ffaa1c17e71e5b74914b/e4b703e2f8b768b6-d6/s400x600/2bcda6d0dfeaffefe9659fbe62aaca57e65da730.gif',
-    },
-  ];
 
   return (
     <Box>
@@ -81,7 +52,7 @@ const MoodForm = (props) => {
             fontSize='16px'
             gutterBottom
           >
-            {user.firstName}'s Mood:
+            {user.firstName}'s Mood for {formattedDate}:
           </Typography>
           {selectedMood && (
             <img
